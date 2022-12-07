1d1446f400b0a4ecb448c3192a28e3d02b3ddebf
RodrigoZea <zearodrigo37@gmail.com>
2022-12-07 01:10:37 -0600

    base HSM implementation from tutorial, need to change it to one that will be used for our needs!

---
+++ PrototipoJVZ/Assets/Scripts/PlayerMovement/StateMachines/Idle.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Idle : Grounded
+{
+    private float _horizontalInput;
+
+    public Idle(MovementSM stateMachine) : base("Idle", stateMachine) { }
+
+    public override void Enter()
+    {
+        base.Enter();
+        _horizontalInput = 0f;
+        //_sm.spriteRenderer.color = Color.black;
+    }
+
+    public override void UpdateLogic()
+    {
+        base.UpdateLogic();
+        _horizontalInput = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(_horizontalInput) > Mathf.Epsilon)
+            stateMachine.ChangeState(_sm.movingState);
+    }
+}
